d44c2ba891fbab87e8408de895c32e2c397c36b0
Gabriel Peña <gapena@ri.gob.do>
2026-03-11 22:14:16 -0400

    Busqueda de los datos

--- app/api/firebase/route.ts
+++ app/api/firebase/route.ts
@@ -1,6 +1,9 @@
+import { ArticleDto } from "@/dtos/articleDto";
+import { initializeApp } from "firebase/app";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
+export const GET = async () => {
 
     const firebaseConfig = {
     apiKey: "AIzaSyDaCiOBKYrLbgfJq6hy0bh9uziRRsfOO-U",
@@ -14,6 +17,51 @@ export const GET = () => {
 
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
 
-    return NextResponse.json({"success": true})
+    interface ArticleHere {
+        id?: string;
+        name: string;
+        price: number;
+        barcode: string;
+        stock: number;
+        minStock: number;
+        maxStock: number;
+        image: string;
+    }
+
+    interface PurchaseOrderHere {
+        id?: string;
+        name: string;
+        address?: string;
+        phoneNumber?: string;
+        email?: string;
+        createdAt?: Date;
+        details: PurchaseOrderDetailHere[];
+    }
+
+    interface PurchaseOrderDetailHere {
+        id?: string;
+        articleId: string;
+        purchaseOrderId?: string;
+        amount: number;
+    }
+
+    let articles: ArticleHere[] = [];
+    let purchaseOrder: PurchaseOrderHere[] = [];
+    let purchaseOrderDetail: PurchaseOrderDetailHere[] = [];
+
+    (await getDocs(collection(db, "article"))).forEach((doc) => {
+        articles.push({ ...doc.data(), id: doc.id} as ArticleHere);
+    });
+
+    (await getDocs(collection(db, "purchaseOrder"))).forEach((doc) => {
+        purchaseOrder.push({ ...doc.data(), id: doc.id} as PurchaseOrderHere);
+    });
+
+    (await getDocs(collection(db, "purchaseOrderDetail"))).forEach((doc) => {
+        purchaseOrderDetail.push({ ...doc.data(), id: doc.id} as PurchaseOrderDetailHere);
+    });
+
+    return NextResponse.json({a: articles[0], po: purchaseOrder[0], pod: purchaseOrderDetail[0]})
 };
